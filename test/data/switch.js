f4511f91f3f06877fa0394c9b9e1c5a36ee8156e
Mortimer333 <michal.dzierzbicki@vp.pl>
2022-04-25 21:17:14 +0200

    Switch Test pass

--- test/data/switch.js
+++ test/data/switch.js
@@ -1,6 +1,6 @@
+let length = 2;
 switch (length) {
-    case 1:
-        console.log('Obj is one')
+    case 1:console.log('Obj is one')
         break;
     default:
         console.log('Obj is bigger then one')
